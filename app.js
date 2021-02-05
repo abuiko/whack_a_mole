4ae4592ab7312c504d34f9c50c3854429fc99c32
Alena Buiko <abuiko.ma@gmail.com>
2021-02-05 13:57:09 -0500

    set global variables

---
+++ app.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector('.grid');
+const square = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.querySelector('#timeLeft');
+const score = document.querySelector('#score');
+
+let result = 0;
